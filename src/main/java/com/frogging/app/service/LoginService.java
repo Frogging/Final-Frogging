8f402d526705a9f5b8468504ee7b95e54b2345fb
imBanseok2 <107028268+imBanseok2@users.noreply.github.com>
2022-10-03 16:48:23 +0900

    관리자 구별

--- src/main/java/com/frogging/app/service/LoginService.java
+++ src/main/java/com/frogging/app/service/LoginService.java
@@ -8,4 +8,5 @@ public interface LoginService {
 	public UserVO loginOk(String id);
 	public int signUpOk(UserVO vo);
 	public int nicknameCheck(String nickname)throws Exception;
+	public int checkAdmin(String id);
 }
